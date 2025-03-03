046796e6fa16c97266732f33aecdc5127c2f797e
gaberielms <gabeof01@gmail.com>
2025-03-02 22:34:42 -0500

    add cats to display

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 import numpy as np
 from PIL import Image
 import io
@@ -7,7 +7,7 @@ from facenet_pytorch import MTCNN, InceptionResnetV1
 
 app = Flask(__name__)
 
-embeddings = {'cat': np.zeros((512,))} # This is just a placeholder
+embeddings = torch.load("static/catname_to_vector.pt")
 
 def load_model():
     mtcnn = MTCNN(keep_all=True)
@@ -20,14 +20,14 @@ def get_image_embedding(image):
     if boxes is None:
         return None
     aligned = mtcnn(image)
-    embeddings = resnet(aligned).detach().numpy()
-    return np.squeeze(embeddings) # Flatten the embeddings to (512,)
+    embeddings = resnet(aligned).detach()
+    return embeddings
 
 def find_best_match(user_embeddings, stuff_embeddings):
     best_match = None
     best_distance = float('inf')
     for key, value in stuff_embeddings.items():
-        distance = np.linalg.norm(user_embeddings - value)
+        distance = (user_embeddings - value).norm().item()
         if distance < best_distance:
             best_match = key
             best_distance = distance
@@ -46,7 +46,7 @@ def index():
         image = Image.open(io.BytesIO(file.read()))
         user_embeddings = get_image_embedding(image)
         best_match = find_best_match(user_embeddings, embeddings)
-        return render_template('result.html', best_match=best_match)
+        return render_template('result.html', meme_cat=best_match)
     return render_template('index.html')
 
 if __name__ == '__main__':
