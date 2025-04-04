25c0777eddf192e4ea74b2529a3b79e4240ea0d0
gaberielms <gabeof01@gmail.com>
2025-04-03 23:53:08 -0400

    working for meme cats

--- app.py
+++ app.py
@@ -1,37 +1,52 @@
 from flask import Flask, request, render_template
-import numpy as np
-from PIL import Image
-import io
-import torch
-from facenet_pytorch import MTCNN, InceptionResnetV1
 
 app = Flask(__name__)
 
-embeddings = torch.load("static/catname_to_vector.pt")
+import face_recognition
+import numpy as np
+import random
+from collections import defaultdict
 
-def load_model():
-    mtcnn = MTCNN(keep_all=True)
-    resnet = InceptionResnetV1(pretrained='casia-webface').eval()
-    return mtcnn, resnet
+embeddings = defaultdict(list) # {img_embedding: int(1-20)}
 
 def get_image_embedding(image):
-    mtcnn, resnet = load_model()
-    boxes, _  = mtcnn.detect(image)
-    if boxes is None:
+    img = face_recognition.load_image_file(image)
+    face_locations = face_recognition.face_locations(img)
+    if len(face_locations) == 0:
         return None
-    aligned = mtcnn(image)
-    embeddings = resnet(aligned).detach()
-    return embeddings
+    face_encodings = face_recognition.face_encodings(img, face_locations)
+    return face_encodings[0]
 
-def find_best_match(user_embeddings, stuff_embeddings):
+def lowest_distance(image):
+    image_embedding = get_image_embedding(image)
+    if image_embedding is None:
+        return "No face detected"
     best_match = None
-    best_distance = float('inf')
-    for key, value in stuff_embeddings.items():
-        distance = (user_embeddings - value).norm().item()
+    best_distance = float("inf")
+    for val, img in embeddings.items():
+        distance = np.linalg.norm(image_embedding - img)
         if distance < best_distance:
-            best_match = key
             best_distance = distance
+            best_match = val
+    return (best_match, best_distance)
+
+def get_best_match(image):
+    best_match, best_distance = lowest_distance(image)
+    if best_match is None:
+        return update_embeddings(get_image_embedding(image))
+    elif best_distance > 0.5:
+        return update_embeddings(get_image_embedding(image))
     return best_match
+
+def update_embeddings(image):
+    val = random.randint(1, 20)
+    if len(embeddings) == 20:
+        embeddings.popitem()
+    while val in embeddings:
+        val = random.randint(1, 20)
+    embeddings[val] = image
+    return val
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -43,10 +58,13 @@ def index():
             return "No selected file", 400
         
         # Read image and convert to processing format
-        image = Image.open(io.BytesIO(file.read()))
+        image = file
         user_embeddings = get_image_embedding(image)
-        best_match = find_best_match(user_embeddings, embeddings)
-        return render_template('result.html', meme_cat=best_match)
+        if user_embeddings is None:
+            return "No face detected", 400
+        best_match = get_best_match(image)
+        cat = 'cat' + str(best_match)
+        return render_template('result.html', meme_cat=cat)
     return render_template('index.html')
 
 if __name__ == '__main__':
